eca2d35564a1f08e1f2f03a3b5f48d1618b2759c
farid fadaie <faridfadaie@farids-MacBook-Pro.local>
2015-05-07 16:54:00 -0700

    no connection pulling

--- test/stress.js
+++ test/stress.js
@@ -1,17 +1,20 @@
 var http = require('http');
 var options = {
-  host: 'bench.bleep.pm',
-  port: 80,
-  path: 'http://bench.bleep.pm/?callback=jQuery1111026272008079104125_1429906480408&i=bleep_stress&e=eyJjYXRlZ29yeSI6Im5vX3JlZmVycmFsIiwiYWN0aW9uIjoiZG93bmxvYWQiLCJsYWJlbCI6Im1hYyIsInVpZCI6ImYzODVkMWEyLWNlZjItNGE5Ny04YTFkLTBkNjRkYzcwNGJlZiJ9&_=142990648040d9'
+  	host: 'bench.bleep.pm',
+  	port: 80,
+	agent:false,
+  	path: 'http://bench.bleep.pm/?callback=jQuery1111026272008079104125_1429906480408&i=bleep_stress&e=eyJjYXRlZ29yeSI6Im5vX3JlZmVycmFsIiwiYWN0aW9uIjoiZG93bmxvYWQiLCJsYWJlbCI6Im1hYyIsInVpZCI6ImYzODVkMWEyLWNlZjItNGE5Ny04YTFkLTBkNjRkYzcwNGJlZiJ9&_=142990648040d9'
 };
 
 function send(cb, er){
 	var now = new Date().getTime();
 	http.get(options, function(res) {
+		res.resume();
 	  res.on('data', function (chunk) {
 		  cb(new Date().getTime() - now);
 	  });
 	}).on('error', function(e) {
+		console.log(e)
 		er(new Date().getTime() - now);
 	});
 }
@@ -23,10 +26,16 @@ function register_time(t){
 	console.log('Got response in ' + t + ' miliseconds. The average so far is '+ sum/res_times.length);
 }
 
-var num_con_calls = 1000000;
-for (var i = 0; i<num_con_calls; i++){
-	send(register_time, function(e){
-		console.log('we hit an error in ' + e + ' miliseconds.');
-	});
+function run_concurrently(n, wait_between_calls){
+	for (var i = 0; i<n; i++){
+		setTimeout(function(){
+			send(register_time, function(e){
+				console.log('we hit an error in ' + e + ' miliseconds.');
+			});
+		}, i * (wait_between_calls || 0));
+	}
 }
+
+var num_con_calls = 10000;
+run_concurrently(num_con_calls, 10);
 
